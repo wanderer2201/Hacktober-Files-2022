7484f7d17797c8b6a0d63be9d8f74886f4f20c41
Trisha7781 <75682219+Trisha7781@users.noreply.github.com>
2022-10-20 22:32:48 +0530

    Bubble sort in javascript

---
+++ JavaScript Codes/bubbleSort.js
@@ -0,0 +1,24 @@
+// Function to sort an array using Bubble sort in js 
+function bubbleSort(arr){
+    
+ for(var i = 0; i < arr.length; i++){
+    
+   for(var j = 0; j < ( arr.length - i -1 ); j++){
+      
+     if(arr[j] > arr[j+1]){
+        
+       var temp = arr[j]
+       arr[j] = arr[j + 1]
+       arr[j+1] = temp
+     }
+   }
+ }
+ console.log(arr);
+}
+ 
+// Unsorted arrat 
+var arr = [234, 43, 55, 63,  5, 6, 235, 547];
+ 
+
+// Sorting array
+bubbleSort(arr);
