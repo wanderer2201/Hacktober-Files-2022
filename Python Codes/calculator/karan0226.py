e20d1841b84687e5d5853151aef70ba7593a9907
Karan0226 <106232809+Karan0226@users.noreply.github.com>
2022-10-23 13:12:57 +0530

    Create karan0226.py
    merge my file hactober fest

---
+++ Python Codes/calculator/karan0226.py
@@ -0,0 +1,22 @@
+#Get the input from user
+print ("Enter Number 1:")
+num1 = int(input())
+ 
+print ("Enter Number 2:")
+num2 = int(input())
+ 
+#Addition
+add = num1 + num2
+print ("Addition: %d" %add)
+ 
+#Subtraction
+sub = num1 - num2
+print ("Subtraction: %d" %sub)
+ 
+#Multiplication
+mul = num1 * num2
+print ("Multiplication: %d" %mul)
+ 
+#Division
+div = num1 / num2
+print ("Division: %.2f" %div)
