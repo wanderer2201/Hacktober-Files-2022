7b1af15c212d844bf466c1be0fe8eae1d59b0edd
RPLD2001 <26rajpreet@gmail.com>
2022-10-30 12:06:19 +0530

    added vector.cpp

---
+++ CPP Codes/vector.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    // Create an empty vector
+    vector<int> vect;
+ 
+    vect.push_back(10);
+    vect.push_back(20);
+    vect.push_back(30);
+ 
+    for (int x : vect)
+        cout << x << " ";
+ 
+    return 0;
+}
