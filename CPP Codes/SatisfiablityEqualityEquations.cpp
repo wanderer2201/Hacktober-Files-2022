4d17c9e9611440fa6453df9407e486f711d0ea0b
Abhiinav710 <95573451+Abhiinav710@users.noreply.github.com>
2022-10-30 15:32:51 +0530

    Create SatisfiablityEqualityEquations.cpp

---
+++ CPP Codes/SatisfiablityEqualityEquations.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    int uf_find(vector<int> &uf, int i) {
+  return uf[i] == -1 || uf[i] == i ? i : uf_find(uf, uf[i]);
+}
+bool equationsPossible(vector<string>& equations) {
+  vector<int> uf('z' + 1, -1);
+  for (auto s : equations)
+    if (s[1] == '=') uf[uf_find(uf, s[0])] = uf_find(uf, s[3]);
+  for (auto s : equations)
+    if (s[1] == '!' && uf_find(uf, s[0]) == uf_find(uf, s[3])) return false;
+  return true;
+}
+};
